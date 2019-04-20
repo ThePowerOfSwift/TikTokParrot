3a409f10af60a0dd4b74790793a127b17f5810cf
Mar Koss <markoss@Mars-MacBook-Pro.local>
2019-04-20 15:53:25 -0700

    Initial Commit

---
+++ TikTokParrot/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TikTokParrot
+//
+//  Created by Mar Koss on 2019-04-20.
+//  Copyright © 2019 Mar Koss. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TikTokParrotTests/TikTokParrotTests.swift
@@ -0,0 +1,34 @@
+//
+//  TikTokParrotTests.swift
+//  TikTokParrotTests
+//
+//  Created by Mar Koss on 2019-04-20.
+//  Copyright © 2019 Mar Koss. All rights reserved.
+//
+
+import XCTest
+@testable import TikTokParrot
+
+class TikTokParrotTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
